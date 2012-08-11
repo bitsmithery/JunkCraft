77966422ffc4f49cef9b8704ace11dae2f2afb79
yuri_kilochek <yuri.kilochek@gmail.com>
2012-08-11 22:16:54 +0400

    put app::color in app::graphics where it belongs

--- app/graphics/color.hpp
+++ app/graphics/color.hpp
@@ -1,25 +1,28 @@
-#ifndef APP_COLOR_HPP
-	#define APP_COLOR_HPP
+#ifndef APP_GRAPHICS_COLOR_HPP
+	#define APP_GRAPHICS_COLOR_HPP
 
 	namespace app
 	{
-		class color
+		namespace graphics
 		{
-			public:
-				float r, g, b, a;
+			class color
+			{
+				public:
+					float r, g, b, a;
+	
+					color() = default;
+					color(color const&) = default;
+					color(float r, float g, float b, float a = 1.0f);
+					color& operator=(color const&) = default;
+					~color() = default;
+			};
 
-				color() = default;
-				color(color const&) = default;
-				color(float r, float g, float b, float a = 1.0f);
-				color& operator=(color const&) = default;
-				~color() = default;
-		};
-
-		inline
-		color::color(float r, float g, float b, float a)
-			: r(r), g(g), b(b), a(a)
-		{
+			inline
+			color::color(float r, float g, float b, float a)
+				: r(r), g(g), b(b), a(a)
+			{
+			}
 		}
 	}
 
-#endif //APP_COLOR_HPP
+#endif
