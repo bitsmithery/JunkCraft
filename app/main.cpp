ceccd0a2e05f0a87d6ac8be513b7460a36cb4cda
yuri_kilochek <yuri.kilochek@gmail.com>
2013-01-28 18:19:47 +0400

    renamed app to bitsmithery

--- app/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "application.hpp"
-
-int main(int argc, char* argv[])
-{
-	bitsmithery::application application(argc, argv);
-	application.run();
-	return application.result();
-}
--- app/system.inl
+++
@@ -1,8 +0,0 @@
-namespace bitsmithery
-{
-	template <typename Engine>
-	system<Engine>::system(Engine& engine)
-		: engine(engine)
-	{
-	}
-}
